2baf5c7752e307825242120ac05615a7d72ca523
felipehsss <felipehsacchi@gmail.com>
2025-02-26 17:24:12 -0300

    first commit

--- aula03/att-programa1.js
+++ aula03/att-programa1.js
@@ -3,6 +3,6 @@ const readline = require('readline-sync');
 const num = Number(readline.question('Digite um numero?'));
 
 
- const result = (!isNaN(num)) ? (num % 2 == 0) ? 'Par' : 'Impar' : 'Inválido';
+const result = (!isNaN(num)) ? (num % 2 === 0) ? 'Par' : 'Impar' : 'Inválido';
 
- console.log(result)
+console.log(`${num} é ${result}`);
